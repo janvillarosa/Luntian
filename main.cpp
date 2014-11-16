0127c5ba0d162b64ab00e8ddbdfda80abaf086c5
micaela-mangubat <micaela_mangubat@dlsu.ph>
2014-11-17 05:48:07 +0800

    Added preprocessing for Biomass
    fix is temporarily placed in main

--- main.cpp
+++ main.cpp
@@ -9,8 +9,10 @@
 #include "Preprocessor.cpp"
 #include "Greenness.cpp"
 #include "Biomass.cpp"
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char *argv[]){
     Mat src;
@@ -22,7 +24,16 @@ int main(int argc, char *argv[]){
     Greenness g_instance;
     g_instance.greenness(filtered);
 
+    Rect roi(600, 2300, 700, 300); //temporary placement
+    Mat src_roi = src(roi);        //crops lower portion of the plant for further processing
+    src_roi.copyTo(filtered);      // processed image will be cropped at the sides using cropImage()
+
+    Mat src_gray;                               //temporary placement
+    cvtColor(filtered, src_gray, CV_RGB2GRAY);  //of this code block
+    threshold(src_gray, filtered, 170, 255, 0); //here. Code runs.
+
     Biomass b_instance;
     b_instance.getPlantWidth(pp_instance.cropImage(filtered));
+    cout << aveWidth << endl;
     waitKey();
 }
