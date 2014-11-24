4a7895236be64baa79cff08d2b2426e13508c32d
janvillarosa <janrobertvillarosa@gmail.com>
2014-11-24 20:30:42 +0800

    Revert d7a94c4..a763708
    This rolls back to commit d7a94c4a24e0455b8e425b4fd4f3e0d022489b9c.

--- main.cpp
+++ main.cpp
@@ -14,9 +14,6 @@
 using namespace cv;
 using namespace std;
 
-const String FILE_DIR = "/Users/janvillarosa/Dropbox/Butil - IRRI Project/Images/10-30-2014/IR64-0";
-
-
 int main(int argc, char *argv[]){
     Mat src;
     double h1[] = { 75.5,67,72.5,76.5,70.5,70,80,83,76,77.5,70,76.5,75.5,63.5,59,65,71.5,73,68.5,70.5,87,90.5,87.5,96.5,0,0,0,0,0,0,0,32,0,0,31,87,94,
@@ -27,15 +24,15 @@ int main(int argc, char *argv[]){
     gfile.open("/Users/janvillarosa/Documents/Luntian/greenness.txt");
 
     for (int i = 1; i < 61; i++){
-        string tempDir = FILE_DIR;
+        string x = "/Users/janvillarosa/Dropbox/Butil - IRRI Project/Images/10-30-2014/IR64-0";
    
         if (i < 10){
-            tempDir.append("0");
+            x.append("0");
         }
-        tempDir.append(to_string(i));
-        tempDir.append(".JPG");
+        x.append(to_string(i));
+        x.append(".JPG");
 
-        src = imread(tempDir, 1);
+        src = imread(x, 1);
         if (src.empty() == false){
             
         
@@ -43,26 +40,26 @@ int main(int argc, char *argv[]){
             pp_instance.setSrc(src);
             Mat wBal = pp_instance.whiteBal();
             
-            tempDir = FILE_DIR; //white-bal image path (for testing purposes)
+            string dir = "/Users/janvillarosa/Documents/Luntian/Pre-processed/IR64-0"; //white-bal image path (for testing purposes)
             if (i < 10){
-                tempDir.append("0");
+                dir.append("0");
             }
-            tempDir.append(to_string(i));
-            tempDir.append("-wbal.JPG");
+            dir.append(to_string(i));
+            dir.append("-wbal.JPG");
             
-            imwrite(tempDir, wBal);
+            imwrite(dir, wBal);
         
             Mat  biomass_segment = pp_instance.bin_segment(wBal);
             Mat  green_segment = pp_instance.noisefilter(pp_instance.rgb_segment(pp_instance.segment(wBal),wBal));
             
-            tempDir = FILE_DIR; //image segmentation
+            dir = "/Users/janvillarosa/Documents/Luntian/Pre-processed/IR64-0"; //image segmentation
             if (i < 10){
-                tempDir.append("0");
+                dir.append("0");
             }
-            tempDir.append(to_string(i));
-            tempDir.append("-rgb.JPG");
+            dir.append(to_string(i));
+            dir.append("-rgb.JPG");
             
-            imwrite(tempDir, green_segment);
+            imwrite(dir, green_segment);
         
         
             Greenness g_instance;
@@ -72,35 +69,33 @@ int main(int argc, char *argv[]){
             
             gfile << "IR64-0" << i << " -- LCC value: " << greenval << " \n";
             
-            tempDir = FILE_DIR; //greenness image output
+            dir = "/Users/janvillarosa/Documents/Luntian/Greenness/IR64-0"; //greenness image output
             if (i < 10){
-                tempDir.append("0");
+                dir.append("0");
             }
-            tempDir.append(to_string(i));
-            tempDir.append(".JPG");
+            dir.append(to_string(i));
+            dir.append(".JPG");
             
-            imwrite(tempDir, green_segment);
+            imwrite(dir, green_segment);
 
             
             
             biomass_segment=pp_instance.cropImage(biomass_segment);
             
-            tempDir = FILE_DIR; //biomass preporcessing image output
+            dir = "/Users/janvillarosa/Documents/Luntian/Pre-processed/IR64-0";
             if (i < 10){
-                tempDir.append("0");
+                dir.append("0");
             }
-            tempDir.append(to_string(i));
-            tempDir.append("-bin.JPG");
+            dir.append(to_string(i));
+            dir.append("-bin.JPG");
             
-            imwrite(tempDir, biomass_segment);
+            imwrite(dir, biomass_segment);
             
             Biomass b_instance;
 
             double aveWidth = b_instance.getPlantWidth(biomass_segment);
-            double biomassval = b_instance.computePlantBiomass(b_instance.convertPixelToCm(b_instance.computePlantRadius(aveWidth)), h1[i - 1]); //biomassfloat output
-            
-            cout << biomassval << endl;
-            myfile << aveWidth << " " << i << ": " << b_instance.computePlantRadius(aveWidth) << " pixels -> " << b_instance.convertPixelToCm(b_instance.computePlantRadius(aveWidth)) << " cm biomass (longest tiller): " << biomassval << " cm \n";
+            cout << b_instance.computePlantBiomass(b_instance.convertPixelToCm(b_instance.computePlantRadius(aveWidth)), h1[i - 1]) << endl;
+            myfile << aveWidth << " " << i << ": " << b_instance.computePlantRadius(aveWidth) << " pixels -> " << b_instance.convertPixelToCm(b_instance.computePlantRadius(aveWidth)) << " cm biomass (longest tiller): " << b_instance.computePlantBiomass(b_instance.convertPixelToCm(b_instance.computePlantRadius(aveWidth)), h1[i - 1]) << " cm \n";
         }
     }
     myfile.close();
